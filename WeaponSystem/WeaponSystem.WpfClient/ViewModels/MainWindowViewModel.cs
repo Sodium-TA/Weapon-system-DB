beb15d53f8a4c4eeb5a7b3b613a14031e7f79e53
Stoyan <stoyan.berov@gmail.com>
2015-10-20 01:40:58 +0300

    Quick spelling mistake fixes

--- WeaponSystem/WeaponSystem.WpfClient/ViewModels/MainWindowViewModel.cs
+++ WeaponSystem/WeaponSystem.WpfClient/ViewModels/MainWindowViewModel.cs
@@ -92,7 +92,7 @@ namespace WeaponSystem.WpfClient.ViewModels
                 var agent = new ZippedXlsToMsSqlAgent();
 
                 var msgWC = await agent.TransferWeapons();
-                this.UniversalButttonText = "Lest's mess with some reports";
+                this.UniversalButttonText = "Let's mess with some reports";
 
                 this.IsUniversalButtonActive = true;
                 this.step = 4;
@@ -112,7 +112,7 @@ namespace WeaponSystem.WpfClient.ViewModels
                 var repo = new MsSqlRepo();
                 var testmsg = await repo.CreteDb();
 
-                this.UniversalButttonText = "Get data from MongpDB";
+                this.UniversalButttonText = "Get data from MongoDB";
                 this.IsUniversalButtonActive = true;
                 this.step = 2;
             }
