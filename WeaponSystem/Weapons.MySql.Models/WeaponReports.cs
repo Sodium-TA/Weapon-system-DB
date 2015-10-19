76e5f87b06408a46858240304f0659ea2ee71843
mr.krustev <mr.krustev1994@gmail.com>
2015-10-20 02:40:23 +0300

    Add MySqlDatabase with EntityFramework implementation. Action namespace provides access to adding JObject reports to the MySql database.

---
+++ WeaponSystem/Weapons.MySql.Models/WeaponReports.cs
@@ -0,0 +1,28 @@
+﻿namespace Weapons.MySqlDb.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class WeaponReports
+    {
+
+        public WeaponReports()
+        {
+        }
+
+        public int ID { get; set; }
+
+        [Required]
+        public int WeaponID { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(50)]
+        public string WeaponName { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(50)]
+        public string Manufacturer { get; set; }
+    }
+}
