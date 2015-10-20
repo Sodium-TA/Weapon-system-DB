b7c2224ca542cb8e0518f8f167cea2ca2a3ea15d
Aleksander Angelov <Obelixx@users.noreply.github.com>
2015-10-20 06:14:05 +0300

    Report to MySQL init

---
+++ WeaponSystem/WeaponSystem.Reports/ReportMySQL.cs
@@ -0,0 +1,53 @@
+﻿namespace WeaponSystem.Reports
+{
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Newtonsoft.Json.Linq;
+    using Weapons.MySqlDb.Actions;
+    using Weapons.MySqlDb.Data;
+    using WeaponSystem.MsSql.Data;
+
+    public class ReportMySQL
+    {
+        private const string MySQLSuccessMessage = "MySQL Reports created successfully.";
+
+        public async Task<string> GenerateMySQLReport()
+        {
+            using (WeaponReportsDbContext MySqlServerContext = new WeaponReportsDbContext())
+            {
+                var list = new List<JObject>();
+                using (WeaponSystemContext msSqlServerContext = new WeaponSystemContext())
+                {
+                    await msSqlServerContext
+                        .Weapons
+                        .Select(w => new
+                        {
+                            Id = w.Id.ToString(),
+                            Name = w.Name.ToString(),
+                            Manufacturer = w.Manufacturer.Name.ToString()
+                        })
+                        .ForEachAsync(w =>
+                            list.Add(CreateJObject(w.Id, w.Name, w.Manufacturer))
+                        );
+                }
+
+                MySqlActions.AddReports(list, MySqlServerContext);
+            }
+
+            return MySQLSuccessMessage;
+        }
+
+        private JObject CreateJObject(string id, string name, string manufacturer)
+        {
+            var jsonObject = new JObject(
+                 new JProperty("weapon-id", id),
+                 new JProperty("weapon-name", name),
+                 new JProperty("manufacturer", manufacturer));
+
+            return jsonObject;
+        }
+    }
+}
